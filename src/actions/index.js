7289a331396984150d53f97af0680bd266a57430
Chi Ho <>
2020-06-28 22:11:45 +0200

    clean up unused code and alter descriptive case

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,27 +2,18 @@ export const types = {
     PASS_WEATHER_DATA: 'PASS_WEATHER_DATA',
     OUTPUT_RESULT: 'OUTPUT_RESULT',
     CITY_NAME: 'CITY_NAME',
-    CHECK_LOAD_DATA: 'CHECK_LOAD_DATA',
-    SUBMIT_INPUT: 'SUBMIT_INPUT'
+    CLEAR_ARRAY: 'CLEAR_ARRAY'
 }
 
-export const submitInput = () => {
-    console.log('submit');
+export const clearArray = () => {
     return {
-        type: types.SUBMIT_INPUT
+        type: types.CLEAR_ARRAY
     }
 }
 
 export const passWeatherData = (data) => {
     return {
         type: types.PASS_WEATHER_DATA,
-        data
-    }
-}
-
-export const outputResult = (data) => {
-    return {
-        type: types.OUTPUT_RESULT,
         data
     }
 }
