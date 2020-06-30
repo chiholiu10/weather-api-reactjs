a0bd1cd27cd596093bbbc1e50db7b78be1be7e4c
Chi Ho <>
2020-07-01 01:30:03 +0200

    created search component testing

---
+++ src/components/search/index.test.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { render, fireEvent } from "@testing-library/react";
+import { Search } from './index';
+import { Provider } from "react-redux";
+import { store } from "../../Store";
+import { screen } from "@testing-library/dom";
+
+const setup = () => {
+    const utils = render(
+        <Provider store={store}>
+            <Search/>
+        </Provider>);
+    const input = utils.getByLabelText("search-input");
+    return {
+        input,
+        ...utils
+    }
+}
+
+test("It should check if input field get the value passed", () => {
+    const { input } = setup();
+    fireEvent.change(input, { target: { value: "search-bar-test" } })
+    expect(input.value).toBe("search-bar-test")
+});
